526c76c6d9cb95baa23ea26b71fb2e1d93bd54c0
Onurege <78586675+onure9e@users.noreply.github.com>
2022-12-04 06:32:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_elseif
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time <= 10){
+                Console.WriteLine("Günaydın!");
+            }
+            else if(time <=12){
+                Console.WriteLine("İyi Günler!");
+            }
+            else{
+                Console.WriteLine("İyi Akşamlar!");
+            }
+        }
+    }
+}
